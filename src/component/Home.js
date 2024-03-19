d45041c09f7cfb3364e3d10e8b2e2607d558a329
danielprinceD <d.danielprince2003@gmail.com>
2024-03-19 15:29:49 +0530

    feat:Completed

---
+++ src/component/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='Homepage'>
+      <div>
+        <h1>Library management system</h1>
+      </div>
+    </div>
+  )
+}
